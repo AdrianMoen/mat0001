a6ec2eb45bc0276d643c79c521a4b83629f52da4
AdrianMoen <adrian.moen01@gmail.com>
2021-10-12 02:19:38 +0200

    successfully printing triangles, but algorithm is off

--- mat0001.py
+++ mat0001.py
@@ -3,15 +3,22 @@
 # Email: Adrian.Moen01@gmail.com
 import sys
 import pygame
+from pygame import color
 from pygame.locals import *
 
 pygame.init()
 
-canvas = pygame.Surface.pygame.Surface.Surface(1280, 720)
+window_width = 1280
+window_height = 720
+
+screencolor = (0, 0, 0)
+
+canvas = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Sierpinski trekant")
 
 class Triangle():
 
-    def __init__(self, x1, y1, x2, y2, x3, y3):
+    def __init__(self, x1, y1, x2, y2, x3, y3, surface):
         self.x1 = x1
         self.y1 = y1
         
@@ -20,12 +27,14 @@ class Triangle():
         
         self.x3 = x3
         self.y3 = y3
+        
+        self.surface = surface
 
-    def draw_triangle(self):
-        pass
+    def draw_triangle(self, color):
 
-    def update_triangle(self):
-        pass
+        pygame.draw.line(self.surface, color, (self.x1, self.y1), (self.x2, self.y2), width=1)
+        pygame.draw.line(self.surface, color, (self.x2, self.y2), (self.x3, self.y3), width=1)
+        pygame.draw.line(self.surface, color, (self.x3, self.y3), (self.x1, self.y1), width=1)
 
     # finner bredden på trekanten
     def get_width(x1, x2, x3):
@@ -74,46 +83,71 @@ class Triangle():
 # gjør sierpinksi delen om til en klasse, mest for at det skal se pent ut
 class SierpinskiTriangle():
     
-    def __init__(self, triangle_list):
+    def __init__(self, triangle_list, n):
         self.triangle_list = triangle_list
+        self.n = n
 
     # rekursiv funksjon som produserer en liste med alle trekantene
-    def calculate_triangle(width, height, triangle_list, n):
+    def calculate_triangles(width, height, triangle_list, n, p):
         
         new_triangle_list = []
 
+        # sjekker om vi har nådd n-te iterasjon, dersom n == 1
+        # returnerer vi den originale trekant listen
+        if n <= 1:
+            return triangle_list
+
         # lager trekanten som forflyttes til høyre
         for triangle in triangle_list:
-            x1New = triangle.x1 + width
-            x2New = triangle.x2 + width
-            x3New = triangle.x3 + width
+            x1New = triangle.x1 + width*p
+            x2New = triangle.x2 + width*p
+            x3New = triangle.x3 + width*p
 
-            newTriangle = Triangle(x1New, triangle.y1, x2New, triangle.y2, x3New, triangle.y3)
+            newTriangle = Triangle(x1New, triangle.y1, x2New, triangle.y2, x3New, triangle.y3, canvas)
             new_triangle_list.append(newTriangle)
             new_triangle_list.append(triangle)
 
         # lager trekanten som forflyttes opp of til høyre i midten
         for triangle in triangle_list:
-            x1New = triangle.x1 + width/2
-            x2New = triangle.x2 + width/2
-            x3New = triangle.x3 + width/2
+            x1New = triangle.x1 + width*p/2
+            x2New = triangle.x2 + width*p/2
+            x3New = triangle.x3 + width*p/2
 
-            y1New = triangle.y1 + height
-            y2New = triangle.y2 + height
-            y3New = triangle.y3 + height
+            y1New = triangle.y1 - height*p
+            y2New = triangle.y2 - height*p
+            y3New = triangle.y3 - height*p
 
-            newTriangle = Triangle(x1New, y1New, x2New, y2New, x3New, y3New)
+            newTriangle = Triangle(x1New, y1New, x2New, y2New, x3New, y3New, canvas)
             new_triangle_list.append(newTriangle)
     
         # fjerner triangle_list for hver gang, siden en ny sendes videre
         del triangle_list
-
         n = n - 1
+        p = p + 1
 
-        if n <= 0:
-            return triangle_list
+        return_list = SierpinskiTriangle.calculate_triangles(width, height, new_triangle_list, n, p)
+        return return_list
 
-        SierpinskiTriangle.calculate_triangle(width, height, new_triangle_list, n)
+    def orient_triangles(triangle_list, width, height, n):
+        
+        for triangle in triangle_list:
+            triangle.x1 += window_width - width/2
+            triangle.x2 += window_width - width/2
+            triangle.x3 += window_width - width/2
+
+            triangle.y1 += window_height - height/2
+            triangle.y2 += window_height - height/2
+            triangle.y3 += window_height - height/2
+            print(triangle.y3)
+
+            triangle.x1 /= 2
+            triangle.x2 /= 2
+            triangle.x3 /= 2
+
+            triangle.y1 /= 2
+            triangle.y2 /= 2
+            triangle.y3 /= 2
+        
 
 
 
@@ -128,30 +162,58 @@ def main():
     x2 = 200
     y2 = 100
 
-    x3 = 50
+    x3 = 150
     y3 = 0
+
+    # fargen til trekantene representert av en tuple med rgb verdier
+    # og en alpha verdi, alle trekantene skal ha samme farge.
+    red = (255, 0, 0, 255)
+    green = (0, 255, 0, 255)
+    blue = (0, 0, 255, 255)
+    cyan = (0, 255, 255, 255)
+    white = (255, 255, 255, 255)
+
+    color = cyan
 
     # finner bredde og høyde, som er statisk gjennom programmet
     width = Triangle.get_width(x1, x2, x3)
     height = Triangle.get_height(y1, y2, y3)
 
+    # henter inn argumentet og gjøre det om til int,
+    # siden den tolkes som en string, eller 'str'
+    argv = sys.argv[0]
+    n = 6
+
+    # lager den første trekanten og legger den inn i den første listen
     triangle_list = []
-
-    # henter inn argumentet
-    n = sys.argv[0]
-
-    #lager den første trekanten og legger den inn i den første listen
-    triangle = Triangle()
+    triangle = Triangle(x1, y1, x2, y2, x3, y3, canvas)
     triangle_list.append(triangle)
 
-    complete_triangle_liste = SierpinskiTriangle.calculate_triangle(width, height, triangle_list, n)
 
+    # lager sierpinski trekanten av n-te grad
+    p = 0
+    complete_triangle_list = SierpinskiTriangle.calculate_triangles(width, height, triangle_list, n, p)
+
+    # initialiserer sierpinski trekanten
+    sierpinski = SierpinskiTriangle(complete_triangle_list, n)
+
+    # forflytter trekantene slik at de er synlige, samt skalerer dem
+    SierpinskiTriangle.orient_triangles(complete_triangle_list, width, height, n)
+
+    # hoved loop for å displaye trekanten
     while running:
+        
+        canvas.fill(screencolor)
 
+        # tegner alle trekantene. Trenger strengt tatt ikke
+        # være inne i hoved loopen, da de ikke flytter på seg
+        for triangle in complete_triangle_list:
+            triangle.draw_triangle(color)
+        # print(len(complete_triangle_list))
         pygame.display.update()
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT():
+            if event.type == pygame.QUIT:
                 runnign = False
                 pygame.display.quit()
                 pygame.quit()
